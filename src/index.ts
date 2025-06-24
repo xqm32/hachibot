c16b036173ef854a2c61fb9ce98026d37e92ee0a
Qiming Xu <33349132+xqm32@users.noreply.github.com>
2025-06-25 00:19:06 +0800

    fix: add CloudflareBindings back

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from "hono";
 import { z } from "zod/v4";
 import { commands } from "./command";
 
-const app = new Hono();
+const app = new Hono<{ Bindings: CloudflareBindings }>();
 
 app.post("/", zValidator("form", z.object({ msg: z.string() })), async (c) => {
   const { msg } = c.req.valid("form");
