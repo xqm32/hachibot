6af165dcea0934a7c901839d422a2168cc17a7c2
Qiming Xu <33349132+xqm32@users.noreply.github.com>
2025-07-10 12:16:01 +0800

    feat: custom model

--- src/llm.ts
+++ src/llm.ts
@@ -3,10 +3,20 @@ import { Command } from "./command";
 import { createGateway } from "@ai-sdk/gateway";
 
 const llm: Command["execute"] = async ({ msg, ref }, c) => {
+  let model: string;
+  let prompt: string;
+
+  const elements = msg.split(" ", 2);
+  if (elements.length < 2) {
+    [model, prompt] = ["xai/grok-3", msg];
+  } else {
+    [model, prompt] = elements;
+  }
+
   const gateway = createGateway({ apiKey: c.env.AI_GATEWAY_API_KEY });
   const { text } = await generateText({
-    model: gateway("xai/grok-3"),
-    prompt: [ref, msg].join("\n"),
+    model: gateway(model),
+    prompt: [ref, prompt].filter(Boolean).join("\n"),
   });
   return text;
 };
