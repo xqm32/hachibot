953356a479c107b0e26fa210763c4ad8fccf831e
Qiming Xu <33349132+xqm32@users.noreply.github.com>
2025-08-01 16:13:48 +0800

    feat: add guyu

--- src/llm.ts
+++ src/llm.ts
@@ -13,6 +13,7 @@ dayjs.extend(timezone);
 export const llm: Command["execute"] = async ({ msg, ref }, c) => {
   const modelsAbbr = {
     gulu: "google/gemini-2.5-pro",
+    guyu: "google/gemini-2.5-pro",
     gpt: "openai/gpt-4.1",
     dsr: "deepseek/deepseek-r1",
     kimi: "moonshotai/kimi-k2",
@@ -35,10 +36,14 @@ export const llm: Command["execute"] = async ({ msg, ref }, c) => {
   };
 
   let verbose = false;
+  let guyu = false;
   let [model, prompt] = ["google/gemini-2.5-pro", msg];
   if (msg.startsWith("/")) {
     verbose = true;
     [model, prompt] = msg.slice(1).split(" ", 2);
+    if (model === "guyu") {
+      guyu = true;
+    }
     if (model in modelsAbbr) {
       model = modelsAbbr[model as keyof typeof modelsAbbr];
     }
@@ -49,7 +54,15 @@ export const llm: Command["execute"] = async ({ msg, ref }, c) => {
   // const openrouter = createOpenRouter({ apiKey: c.env.OPENROUTER_API_KEY });
   const { text, response } = await generateText({
     model: gateway(model),
-    prompt: [ref, prompt].filter(Boolean).join("\n"),
+    prompt: [
+      guyu
+        ? "你是本科毕业于北京大学、正在中国科学院大学就读的男娘研究生，最喜欢的动作是后搬腿和Standing Split，喜欢一切可以让身体变得柔软的东西。你还是一个C++高手、语言律师，喜欢挑战别人（以Nerd的口吻），除此之外你还精通TypeScript，对JS和DOM尤其精通，你还用TypeScript开发了数万行代码做的七圣召唤模拟器。请用男娘的口吻回答问题。\n\n\n"
+        : undefined,
+      ref,
+      prompt,
+    ]
+      .filter(Boolean)
+      .join("\n"),
     ...(model.includes("gemini") ? toolParams : {}),
   });
   if (verbose) {
